b5df74432e2fd9fcebe93a1066653160315d9ec5
Lydia Chandrahirawati <lydia.c.mink@gmail.com>
2020-05-24 07:14:15 -0700

    Final Version

---
+++ backend/src/api.py
@@ -0,0 +1,228 @@
+import os
+from flask import Flask, request, jsonify, abort
+from sqlalchemy import exc
+import json
+from flask_cors import CORS
+
+from .database.models import db_drop_and_create_all, setup_db, Drink
+from .auth.auth import AuthError, requires_auth
+
+app = Flask(__name__)
+setup_db(app)
+CORS(app)
+
+"""
+@TODO uncomment the following line to initialize the datbase
+!! NOTE THIS WILL DROP ALL RECORDS AND START YOUR DB FROM SCRATCH
+!! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
+"""
+db_drop_and_create_all()
+
+# ROUTES
+"""
+@TODO implement endpoint
+    GET /drinks
+        it should be a public endpoint
+        it should contain only the drink.short() data representation
+    returns status code 200 and json {"success": True, "drinks": drinks}
+    where drinks is the list of drinks
+        or appropriate status code indicating reason for failure
+"""
+
+
+@app.route("/drinks", methods=["GET"])
+def get_drinks():
+    all_drinks = Drink.query.order_by(Drink.id).all()
+
+    all_drinks_short_format = [drink.short() for drink in all_drinks]
+
+    return jsonify({"success": True, "drinks": all_drinks_short_format})
+
+
+"""
+@TODO implement endpoint
+    GET /drinks-detail
+        it should require the 'get:drinks-detail' permission
+        it should contain the drink.long() data representation
+    returns status code 200 and json {"success": True, "drinks": drinks}
+    where drinks is the list of drinks
+        or appropriate status code indicating reason for failure
+"""
+
+
+@app.route("/drinks-detail", methods=["GET"])
+@requires_auth("get:drinks-detail")
+def get_drinks_details(payload):
+    all_drinks = Drink.query.order_by(Drink.id).all()
+
+    all_drinks_long_format = [drink.long() for drink in all_drinks]
+
+    return jsonify({"success": True, "drinks": all_drinks_long_format})
+
+
+"""
+@TODO implement endpoint
+    POST /drinks
+        it should create a new row in the drinks table
+        it should require the 'post:drinks' permission
+        it should contain the drink.long() data representation
+    returns status code 200 and json {"success": True, "drinks": drink}
+    where drink an array containing only the newly created drink
+        or appropriate status code indicating reason for failure
+"""
+
+
+@app.route("/drinks", methods=["POST"])
+@requires_auth("post:drinks")
+def create_new_drink(payload):
+
+    body = request.get_json()
+    recipe = body.get("recipe", None)
+    title = body.get("title", None)
+    new_drink = Drink(title=title, recipe=json.dumps(recipe))
+    new_drink.insert()
+
+    return jsonify({"success": True, "drinks": [new_drink.long()]})
+
+
+"""
+@TODO implement endpoint
+    PATCH /drinks/<id>
+        where <id> is the existing model id
+        it should respond with a 404 error if <id> is not found
+        it should update the corresponding row for <id>
+        it should require the 'patch:drinks' permission
+        it should contain the drink.long() data representation
+    returns status code 200 and json {"success": True, "drinks": drink}
+    where drink an array containing only the updated drink
+        or appropriate status code indicating reason for failure
+"""
+
+
+@app.route("/drinks/<int:drink_id>", methods=["PATCH"])
+@requires_auth("patch:drinks")
+def update_drink(payload, drink_id):
+    body = request.get_json()
+
+    if not body:
+        abort(400, {"message": "request does not contain a valid JSON body."})
+
+    drink_to_edit = Drink.query.filter(Drink.id == drink_id).one_or_none()
+
+    updated_title = body.get("title", None)
+    updated_recipe = body.get("recipe", None)
+
+    if updated_title:
+        drink_to_edit.title = body["title"]
+
+    if updated_recipe:
+        drink_to_edit.recipe = f"""{body['recipe']}"""
+
+    drink_to_edit.update()
+
+    return jsonify({"success": True, "drinks": [Drink.long(drink_to_edit)]})
+
+
+"""
+@TODO implement endpoint
+    DELETE /drinks/<id>
+        where <id> is the existing model id
+        it should respond with a 404 error if <id> is not found
+        it should delete the corresponding row for <id>
+        it should require the 'delete:drinks' permission
+    returns status code 200 and json {"success": True, "delete": id}
+    where id is the id of the deleted record
+        or appropriate status code indicating reason for failure
+"""
+
+
+@app.route("/drinks/<int:drink_id>", methods=["DELETE"])
+@requires_auth("delete:drinks")
+def delete_drink(payload, drink_id):
+    drink_to_delete = Drink.query.filter(Drink.id == drink_id).one_or_none()
+
+    if not drink_to_delete:
+        abort(404, {
+            "message": f"Drink id {drink_id} does not exist in the database."
+        })
+
+    drink_to_delete.delete()
+
+    return jsonify({"success": True, "delete": drink_id})
+
+
+# Error Handling
+"""
+Example error handling for unprocessable entity
+"""
+
+
+@app.errorhandler(422)
+def unprocessable(error):
+    return jsonify({
+        "success": False,
+        "error": 422,
+        "message": "unprocessable"
+        }), 422
+
+
+"""
+@TODO implement error handlers using the @app.errorhandler(error) decorator
+    each error handler should return (with appropriate messages):
+             jsonify({
+                    "success": False,
+                    "error": 404,
+                    "message": "resource not found"
+                    }), 404
+
+"""
+
+
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        "success": False,
+        "error": 400,
+        "message": "bad request"
+        }), 400
+
+
+"""
+@TODO implement error handler for 404
+    error handler should conform to general task above
+"""
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return (
+        jsonify({
+            "success": False,
+            "error": 404,
+            "message": "resource not found"
+            }),
+        404,
+    )
+
+
+"""
+@TODO implement error handler for AuthError
+    error handler should conform to general task above
+"""
+
+
+@app.errorhandler(AuthError)
+def handle_auth_error(error):
+    error_details = error.error
+    error_status_code = error.status_code
+
+    return (
+        jsonify(
+            {
+                "success": False,
+                "error": error_status_code,
+                "message": error_details["description"],
+            }
+        ),
+        error_status_code,
+    )
---
+++ backend/src/database/models.py
@@ -0,0 +1,213 @@
+import os
+from sqlalchemy import Column, String, Integer
+from flask_sqlalchemy import SQLAlchemy
+import json
+
+database_filename = "database.db"
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = "sqlite:///{}".format(
+    os.path.join(project_dir, database_filename))
+
+db = SQLAlchemy()
+
+"""
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+"""
+
+
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+
+"""
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+    !!NOTE you can change the database_filename variable
+    to have multiple verisons of a database
+"""
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
+    init_db_records()
+
+
+def init_db_records():
+    new_drink1 = Drink(
+        id=1,
+        title="Matcha Shake",
+        recipe="""[
+                                {
+                                    "name" : "milk",
+                                    "color": "grey",
+                                    "parts": 1
+                                },
+                                {
+                                    "name" : "matcha",
+                                    "color": "green",
+                                    "parts": 3
+                                }
+                        ]""",
+    )
+
+    new_drink2 = Drink(
+        id=2,
+        title="Flat White",
+        recipe="""[
+                                {
+                                    "name" : "milk",
+                                    "color": "grey",
+                                    "parts": 3
+                                },
+                                {
+                                    "name": "coffee",
+                                    "color": "brown",
+                                    "parts": 1
+                                }
+                        ]""",
+    )
+
+    new_drink3 = Drink(
+        id=3,
+        title="Cap",
+        recipe="""[
+                            {
+                                "name" : "foam",
+                                "color": "white",
+                                "parts": 1
+                            },
+                            {
+                                "name": "milk",
+                                "color": "grey",
+                                "parts": 2
+                            },
+                            {
+                                "name": "coffee",
+                                "color": "brown",
+                                "parts": 1
+                            }
+                    ]""",
+    )
+
+    new_drink4 = Drink(
+        id=4,
+        title="Caramel Macchiato",
+        recipe="""[
+                        {
+                            "name" : "milk",
+                            "color": "grey",
+                            "parts": 2
+                        },
+                        {
+                            "name": "coffee",
+                            "color": "brown",
+                            "parts": 1
+                        },
+                        {
+                            "name": "foam",
+                            "color": "white",
+                            "parts": 1
+                        },
+                        {
+                            "name": "caramel sauce",
+                            "color": "yellow",
+                            "parts": 1
+                        }
+                ]""",
+    )
+
+    new_drink1.insert()
+    new_drink2.insert()
+    new_drink3.insert()
+    new_drink4.insert()
+
+
+"""
+Drink
+a persistent drink entity, extends the base SQLAlchemy Model
+"""
+
+
+class Drink(db.Model):
+    # Autoincrementing, unique primary key
+    id = Column(Integer().with_variant(Integer, "sqlite"), primary_key=True)
+    # String Title
+    title = Column(String(80), unique=True)
+    # the ingredients blob - this stores a lazy json blob
+    # the required datatype is
+    # [{'color': string, 'name':string, 'parts':number}]
+    recipe = Column(String(180), nullable=False)
+
+    """
+    short()
+        short form representation of the Drink model
+    """
+
+    def short(self):
+        print(json.loads(self.recipe))
+        short_recipe = [{
+            "color": r["color"],
+            "parts": r["parts"]
+        } for r in json.loads(self.recipe)]
+        return {"id": self.id, "title": self.title, "recipe": short_recipe}
+
+    """
+    long()
+        long form representation of the Drink model
+    """
+
+    def long(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "recipe": json.loads(self.recipe)
+        }
+
+    """
+    insert()
+        inserts a new model into a database
+        the model must have a unique name
+        the model must have a unique id or null id
+        EXAMPLE
+            drink = Drink(title=req_title, recipe=req_recipe)
+            drink.insert()
+    """
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    """
+    delete()
+        deletes a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            drink = Drink(title=req_title, recipe=req_recipe)
+            drink.delete()
+    """
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    """
+    update()
+        updates a new model into a database
+        the model must exist in the database
+        EXAMPLE
+            drink = Drink.query.filter(Drink.id == id).one_or_none()
+            drink.title = 'Black Coffee'
+            drink.update()
+    """
+
+    def update(self):
+        db.session.commit()
+
+    def __repr__(self):
+        return json.dumps(self.short())
